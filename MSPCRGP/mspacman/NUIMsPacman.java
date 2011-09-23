6524df055f4cbf9168c3a7a1522b0a5adf3c1a72
Bjarte Johansen <bjo013@uib.no>
2011-09-23 15:35:54 +0200

    Another commit for the move.

--- MSPCRGP/mspacman/NUIMsPacman.java
+++ MSPCRGP/mspacman/NUIMsPacman.java
@@ -1,32 +1,32 @@
-package cottage;
+package mspacman;
 
-import java.awt.event.KeyEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import jef.machine.Machine;
 import jef.util.Throttle;
+import cottage.CottageDriver;
 
-public class MsPacmanGame implements Runnable {
+public class NUIMsPacman implements Runnable, MsPacman {
 	private static String VERSION = "0.01a";
 	private int[] pixel;
 	private Machine m;
 	private Throttle t;
 	private boolean stop = false;
-	
-	
+
 	@Override
 	public void run() {
 		String driver = "";
 		System.out.println("Version " + VERSION);
-		
+
 		URL base_URL = null;
 		try {
-			base_URL = new URL("file://localhost/Users/bjarte/Documents/workspace/mspacman/bin/");
+			base_URL = new URL(
+					"file://localhost/Users/bjarte/Documents/workspace/mspacman/bin/");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		
+
 		driver = "mspacman";
 		boolean sound = false;
 
@@ -52,71 +52,52 @@ public class MsPacmanGame implements Runnable {
 			t.throttle();
 		}
 	}
-	
+
+	@Override
 	public int[] getPixels() {
 		return pixel;
 	}
-	
+
+	@Override
 	public void keyPressed(int keyCode) {
 		m.keyPress(keyCode);
 	}
-	
+
+	@Override
 	public void keyReleased(int keyCode) {
 		m.keyRelease(keyCode);
 	}
-	
+
+	@Override
 	public long getScore() {
-		return getScore(0x43ed, ((cottage.machine.Pacman)m).md.getREGION_CPU());
+		return getScore(0x43ed, ((cottage.machine.Pacman) m).md.getREGION_CPU());
+		// score = 0x43f7, highscore = 0x43ed
 	}
-	
+
 	private long getScore(int offset, int[] mem) {
 		final int ZERO_CHAR = 0x00;
 		final int BLANK_CHAR = 0x40;
-		
+
 		long score = 0;
-		
+
 		// calculate the score
 		for (int i = 0; i < 7; i++) {
 			int c = mem[offset + i];
-			if (c == 0x00 || c == BLANK_CHAR) c = ZERO_CHAR;
+			if (c == 0x00 || c == BLANK_CHAR)
+				c = ZERO_CHAR;
 			c -= ZERO_CHAR;
 			score += (c * Math.pow(10, i));
 		}
-		
+
 		return (score > 9999999) ? 0 : score;
 	}
-	
+
 	public void stop(boolean stop) {
 		this.stop = stop;
 	}
-	
-	public static void main(String args[]) throws InterruptedException {
-		MsPacmanGame g = new MsPacmanGame();
-		Thread t = new Thread(g);
-		t.start();
-		Thread.sleep(6000);
-		g.keyPressed(KeyEvent.VK_5);
-		System.out.println("####5#####");
-		Thread.sleep(500);
-		g.keyReleased(KeyEvent.VK_5);
-		System.out.println("@@@@release 5@@@@");
-		Thread.sleep(500);
-		System.out.println("@@@@@1@@@@@");
-		g.keyPressed(KeyEvent.VK_1);
-		Thread.sleep(100);
-		g.keyReleased(KeyEvent.VK_1);
-		System.out.println("@@@@@release 1@@@@");
-		Thread.sleep(200);
-		System.out.println("@@@@@LEFT@@@@@@@");
-		g.keyPressed(KeyEvent.VK_LEFT);
-		for(int i = 0; i < 30; ++i) {
-			Thread.sleep(1000);
-			System.out.println(i);
-		}
-		System.out.println(g.getScore() + " " + t.isAlive());
-		g.stop(true);
-		Thread.sleep(1000);
-		System.out.println(t.isAlive());
-		
+
+	@Override
+	public boolean isGameOver() {
+		return ((cottage.machine.Pacman) m).md.getREGION_CPU()[0x403B] == 67;
 	}
 }
