f0ddb197f17d7902a5b3906e22b52898143339b4
Bjarte Johansen <bjo013@uib.no>
2011-09-23 15:35:25 +0200

    Moved some classes to make it easier to handle everything
    Moved all new code that will be run by the GP framework into a new
    source folder.
    Moved tests into a separate class.

---
+++ MSPCRGP/mspacman/test/NUIMsPacmanTest.java
@@ -0,0 +1,36 @@
+package mspacman.test;
+
+import java.awt.event.KeyEvent;
+
+import mspacman.NUIMsPacman;
+
+public class NUIMsPacmanTest {
+	public static void main(String args[]) throws InterruptedException {
+		NUIMsPacman g = new NUIMsPacman();
+		Thread t = new Thread(g);
+		t.start();
+		Thread.sleep(6000);
+		g.keyPressed(KeyEvent.VK_5);
+		System.out.println("####5#####");
+		Thread.sleep(500);
+		g.keyReleased(KeyEvent.VK_5);
+		System.out.println("@@@@release 5@@@@");
+		Thread.sleep(500);
+		System.out.println("@@@@@1@@@@@");
+		g.keyPressed(KeyEvent.VK_1);
+		Thread.sleep(100);
+		g.keyReleased(KeyEvent.VK_1);
+		System.out.println("@@@@@release 1@@@@");
+		Thread.sleep(200);
+		System.out.println("@@@@@LEFT@@@@@@@");
+		g.keyPressed(KeyEvent.VK_LEFT);
+		for (int i = 0; i < 30; ++i) {
+			Thread.sleep(1000);
+			System.out.println(i);
+		}
+		System.out.println(g.getScore() + " " + t.isAlive());
+		g.stop(true);
+		Thread.sleep(1000);
+		System.out.println(t.isAlive());
+	}
+}
