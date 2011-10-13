f74fc6d389eae5fde111fc6df01ab966668fe364
Bjarte Johansen <bjo013@uib.no>
2011-10-13 17:55:10 +0200

    First test if clojure. I can now run Ms Pacman GUI from clojure.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mspacman "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :java-source-path [["CottAGE_1.6_src/"]
+                     ["MSPCRGP/"]]
+  :main mspacman.core)
---
+++ test/mspacman/test/core.clj
@@ -0,0 +1,6 @@
+(ns mspacman.test.core
+  (:use [mspacman.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
