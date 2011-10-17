c8dbae77e90af94153ebea1ba8a3eacf52290ccd
Bjarte Johansen <bjo013@uib.no>
2011-10-17 15:30:12 +0200

    Start of clojure code

--- src/mspacman/core.clj
+++ src/mspacman/core.clj
@@ -1,11 +1,13 @@
 (ns mspacman.core)
 
-(import mspacman.GUIMsPacman)
+(import '(no.uib.bjo013.mspacman MsPacman GUIMsPacman NUIMsPacman))
 (import javax.swing.JFrame)
 (import java.awt.BorderLayout)
 (import java.lang.Boolean)
+(import java.awt.event.KeyEvent)
 
-(defn start-GUIMsPacman [& args]
+
+(defn start-GUIMsPacman []
   (let [msp (doto (new GUIMsPacman)
               (.setSize 224 (+ 288 22)))
         frame (doto (new JFrame)
@@ -16,5 +18,40 @@
                 (.setVisible (. Boolean TRUE)))]
     (-> (new Thread msp) .start)))
 
+(defn start-NUIMsPacman-test []
+  (let [msp (new NUIMsPacman)
+        t (new Thread msp)]
+    (-> t .start)
+    (dotimes [n 6]
+      (Thread/sleep 1000)
+      (println n))
+    (-> msp (.keyPressed KeyEvent/VK_5))
+    (println "#### pressed 5 ####")
+    (Thread/sleep 500)
+    (-> msp (.keyReleased KeyEvent/VK_5))
+    (println "#### released 5")
+    (Thread/sleep 500)
+    (-> msp (.keyPressed KeyEvent/VK_1))
+    (println "#### pressed 1 ####")
+    (Thread/sleep 500)
+    (-> msp (.keyReleased KeyEvent/VK_1))
+    (println "#### released 1")
+    (Thread/sleep 500)
+    (-> msp (.keyPressed KeyEvent/VK_LEFT))
+    (println "#### pressed LEFT ####")
+    (Thread/sleep 500)
+    (-> msp (.keyReleased KeyEvent/VK_LEFT))
+    (println "#### released LEFT")
+    (Thread/sleep 500)
+    (dotimes [i 30]
+      (Thread/sleep 1000)
+      (println i))
+    (print (-> msp .getScore))
+    (print " ")
+    (println (-> t .isAlive))
+    (-> msp  (.stop  Boolean/TRUE))
+    (Thread/sleep 1000)
+    (println (-> t .isAlive))))
+
 (defn -main [& args]
-  (start-GUIMsPacman))
+  (start-NUIMsPacman-test))
