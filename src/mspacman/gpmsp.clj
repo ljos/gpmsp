e069443c21cd404c9d646bb055d410af949d906a
Bjarte Johansen <bjo013@uib.no>
2011-10-19 18:17:13 +0200

    Finished the fitness-function and added som functions for the gp to use.

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -10,23 +10,12 @@
   :fitness
   :finishing-time)
 
-(def population-size  (atom 30))
-(def fitness-tries (atom 5))
 
-(defn fitness [code msp]
-  (let [t (new Thread msp)]
-    (-> t .start)
-    (Thread/sleep 6000)
-    (loop [n @fitness-tries]
-      (if (< n 1)
-        nil
-        (while (not (-> msp isGameOver))
-          (eval code))
-        (recur (dec n))))
-    (let [fitness-score (-> msp .getScore)]
-      (-> msp .stop Boolean/TRUE)
-      fitness-score)))
 
 (defn create-random-population []
   (dotimes [@population-size n]
     ()))
+
+
+(defn -main [& args]
+  ())
