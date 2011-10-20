2e950965f126e6a95123d534dceb21e45724525b
Bjarte Johansen <bjo013@uib.no>
2011-10-20 19:01:40 +0200

    Added an empty create-random-individual

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -2,12 +2,16 @@
 
 (import java.awt.event.KeyEvent)
 (import java.lang.Boolean)
+(import mspacman.individual)
 
 (defstruct individual
   :program
   :mspacman
   :fitness
   :finishing-time)
+
+(defn create-random-individual []
+  ())
 
 (defn create-random-population []
   (dotimes [@population-size n]
