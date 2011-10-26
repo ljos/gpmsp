e4e1b752b24bed0e64851d88eb4969a00d87d0e5
Bjarte Johansen <bjo013@uib.no>
2011-10-26 14:18:28 +0200

    Expand doesn't work properly, putting into vc to have it saved for the future

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -1,8 +1,8 @@
-(ns mspacman.gpmsp)
+(ns mspacman.gpmsp
+  (:use (mspacman.individual))
+  (:import (java.awt.event.KeyEvent)
+           (java.lang.Boolean)))
 
-(import java.awt.event.KeyEvent)
-(import java.lang.Boolean)
-(import mspacman.individual)
 
 (defstruct individual
   :program
@@ -10,13 +10,63 @@
   :fitness
   :finishing-time)
 
+(def *SIZE-OF-POPULATION* 50)
+(def *NUMBER-OF-GENERATIONS* 1000)
+(def *MAX-STARTING-DEPTH* 10)
+(def *MAX-WIDTH-OF-EXPR* 10)
+(def *MAX-DEPTH* 10)
+
+
 (defn create-random-individual []
-  ())
+  (expand '(do expr+) *MAX-DEPTH*))
+
+(defn- expand [exprs depth]
+  (println exprs)
+  (if (or (not (coll? exprs)) (< (count exprs) 1) (<= depth 0)) 
+    (if (or (empty exprs)
+            (some #(or (= 'expr %1)
+                       (= 'expr+ %1)
+                       (= 'expr? %1)
+                       (= 'int %1)
+                       (nil? %1)) exprs))
+      ()
+      exprs)
+    (cons (first exprs)
+          (apply concat 
+                 (for [term  (rest exprs)]
+                   (do (print 'term)
+                       (println term)
+                       (case term
+                         expr  (list (let [t (rand-nth mspacman.individual/FUNCTION-LIST)]
+                                       (do (print 'expr)
+                                           (println t)
+                                           (expand (if (= t 'int) (list t) t) (dec depth)))))
+                         expr+ (for [t (take (inc (rand-int *MAX-WIDTH-OF-EXPR*))
+                                             (repeatedly
+                                              #(rand-nth mspacman.individual/FUNCTION-LIST)))]
+                                 (do (print 'expr+)
+                                     (println t)
+                                     (if (= t 'int)
+                                       (list 'int)
+                                       (let [e (expand t (dec depth))]
+                                         (print e)
+                                         (println e)
+                                         (if (every? empty (rest e))
+                                           ()
+                                           e)))))
+                         expr? (if (rand-nth '(true false))
+                                 (list (expand (rand-nth mspacman.individual/FUNCTION-LIST)
+                                               (dec depth)))
+                                 ())
+                         int   (list (rand-int 1000))
+                         (do (print 'default)
+                             (println term)
+                              (list term)))))))))
 
 (defn create-random-population []
-  (dotimes [@population-size n]
-    ()))
+  (apply pcalls (repeat #(create-random-individual))))
 
 
 (defn -main [& args]
   ())
+
