b55e110eef4d95a92d8c908c0921103d110dc69d
Bjarte Johansen <bjo013@uib.no>
2011-10-27 18:43:27 +0200

    The auto generated code now works with the GUI runner. This means it should also work with the NUI as well.
    I had to change a few things to get this to work. Now some of the
    functions take inn the msp-object, but that works out fine as long as
    it is remembered to add it to the  end of the list as the expand
    function reverses the sequence of the function.

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -16,36 +16,37 @@
 (def *MAX-WIDTH-OF-EXPR* 5)
 (def *MAX-DEPTH* 10)
 
+(defn expand [exprs depth]
+  (cond (= exprs 'int)
+        ,(rand-int 1000)
+        (or (empty? exprs) (< depth 1))
+        ,()
+        :else
+        ,(cons (first exprs) 
+               (loop [terms (rest exprs)
+                      acc ()
+                      expr-width (rand-int *MAX-WIDTH-OF-EXPR*)]
+                 (if (empty? terms)
+                   acc
+                   (let [term (first terms)
+                         exp (case term
+                               (expr expr+) (expand (rand-nth mspacman.individual/FUNCTION-LIST)
+                                                    (dec depth))
+                               expr? (if (< (rand) 0.50)
+                                       (expand (rand-nth mspacman.individual/FUNCTION-LIST)
+                                               (dec depth))
+                                       ())
+                               int (expand term (dec depth))
+                               `~term)]
+                     (recur (if (and (= term 'expr+)
+                                     (> expr-width 0))
+                              terms
+                              (rest terms))
+                            (cons exp acc)
+                            (dec expr-width))))))))
 
 (defn create-random-individual []
   (expand '(do expr+) *MAX-DEPTH*))
-
-(defn expand [exprs depth]
-  (if (or (= exprs 'int) (empty? exprs) (< depth 1))
-    (if (= exprs 'int)
-      (rand-int 1000)
-      ())
-    (cons  (first exprs)
-           (loop [terms (rest exprs)
-                  acc ()
-                  expr-width (rand-int *MAX-WIDTH-OF-EXPR*)]
-             (if  (or (empty? terms) (< depth 1))
-               acc
-               (let  [exp (case (first terms)
-                             (expr expr+) (expand (rand-nth mspacman.individual/FUNCTION-LIST)
-                                                  (dec depth))
-                             expr? (if (< (rand) 0.50)
-                                     (expand (rand-nth mspacman.individual/FUNCTION-LIST)
-                                             (dec depth))
-                                     ())
-                             int (rand-int 1000)
-                             (first terms))]
-                 (recur (if (and (= (first terms) 'expr+)
-                                 (> expr-width 0))
-                          terms
-                          (rest terms))
-                        (cons exp acc)
-                        (dec expr-width))))))))
 
 (defn create-random-population []
   (apply pcalls (repeat *SIZE-OF-POPULATION* #(create-random-individual))))
