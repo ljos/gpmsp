e0c2f35c5d2f6f687812aeb8afb721badbf33f20
Bjarte Johansen <bjo013@uib.no>
2011-11-03 19:54:30 +0100

    Now runs with lein run

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :java-source-path [["CottAGE_1.6_src/"]
                      ["MSPCRGP/"]]
+  :java-opts ["-server"
+              "-d64"
+              "-Xmx2048M"
+              "-Xms2048M"
+              "-XX:MaxPermSize=256M"]
   :main mspacman.core)
