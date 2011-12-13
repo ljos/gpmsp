84dfe665b6b35d43ffd331aef674a9b3a749dc5f
Bjarte Johansen <bjo013@uib.no>
2011-12-13 13:54:07 +0100

    Added 3 more rooms
    3 more rooms added as clusters

--- control.clj
+++ control.clj
@@ -40,5 +40,77 @@
               "mn121075"	
               "mn121077"])
 
+(defcluster :H3
+  :parallel true
+  :user "bjo013"
+  :addresses ["mn121083"	
+              "mn121084"	
+              "mn121085"	
+              "mn121086"	
+              "mn121087"	
+              "mn121088"	
+              "mn121089"	
+              "mn121090"	
+              "mn121091"	
+              "mn121092"
+              "mn121093"	
+              "mn121094"	
+              "mn121095"	
+              "mn121096"	
+              "mn121097"	
+              "mn121098"	
+              "mn121099"	
+              "mn121100"	
+              "mn121101"	
+              "mn121102"	
+              "mn121103"	
+              "mn121104"	
+              "mn121105"	
+              "mn121107"])
+
+(defcluster :RF1
+  :parallel true
+  :user "bjo013"
+  :addresses ["mn190142"	
+              "mn190143"	
+              "mn190144"	
+              "mn190145"	
+              "mn190146"	
+              "mn190147"	
+              "mn190148"	
+              "mn190149"	
+              "mn190150"	
+              "mn190151"	
+              "mn190152"	
+              "mn190153"	
+              "mn190154"	
+              "mn190155"	
+              "mn190156"	
+              "mn190157"
+              "mn190070"])
+
+(defcluster :RF3
+  :parallel true
+  :user "bjo013"
+  :addresses ["mnr3-190117"	
+              "mnr3-190118"	
+              "mnr3-190119"	
+              "mnr3-190120"	
+              "mnr3-190121"	
+              "mnr3-190122"	
+              "mnr3-190123"	
+              "mnr3-190124"	
+              "mnr3-190125"	
+              "mnr3-190126"	
+              "mnr3-190127"	
+              "mnr3-190128"	
+              "mnr3-190129"	
+              "mnr3-190130"	
+              "mnr3-190134"	
+              "mnr3-190136"	
+              "mnr3-190138"	
+              "mnr3-190139"
+              "mnr3-190140"])
+
 (deftask :date "echo date on cluster"  []
   (ssh "date"))
