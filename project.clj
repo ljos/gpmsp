328618ed92e3e9fde93a59f97a00f5a85f244b42
Bjarte Johansen <bjo013@uib.no>
2011-12-15 18:37:52 +0100

    Added server java opt, updated dependency

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "Genetic programming of mspacman bot"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.zip "0.1.0"]]
-  :dev-dependencies [[control "0.2.3-SNAPSHOT"]]
+  :dev-dependencies [[control "0.2.4-SNAPSHOT"]]
   :java-source-path [["CottAGE_1.6_src/"]
                      ["MSPCRGP/"]]
-  :java-opts ["-Xmx2G"
+  :java-opts ["-server"
+              "-Xmx2G"
               "-Xms2G"
               "-Xmn1G"
               "-XX:MaxPermSize=256M"
