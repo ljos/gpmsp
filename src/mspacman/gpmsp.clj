f783b4e5b8f1a93f4b5ada9ad9c9c03d9f71030d
Bjarte Johansen <bjo013@uib.no>
2012-01-11 18:06:05 +0100

    removed checking of hostfile

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -231,7 +231,7 @@
 
 (defn distribute [machine]
   (spawn (into-array String
-                     ["ssh" "-o ConnectTimeout=2"
+                     ["ssh" "-o ConnectTimeout=2  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
                       (format "bjo013@%s" machine)
                       "hostname"])))
 
