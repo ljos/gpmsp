b4a749243c2dc6a00d748fc43ed06f2570e3046e
Bjarte Johansen <bjo013@uib.no>
2012-01-18 15:37:44 +0100

    forgot this was removed already

--- src/mspacman/core.clj
+++ src/mspacman/core.clj
@@ -5,8 +5,6 @@
 (defn -main [& args]
   (cond (empty? args)
         ,(gp/gp-run)
-        (= 1 (count args))
-        ,(gp/contrl)
         :else
         (gp/gp-run (first args)
                    (second args))))
