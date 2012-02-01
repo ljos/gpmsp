cfa672d9067d5212a55d16a6e9dee556053e8d1a
Bjarte Johansen <bjo013@uib.no>
2012-02-01 12:00:45 +0100

    Removed printout from control

--- src/mspacman/control.clj
+++ src/mspacman/control.clj
@@ -59,7 +59,6 @@
               (fn [exec-process]
                 (assoc exec-process :stderr (str (:stderr exec-process)
                                                  (join "\r\n" (doall (line-seq err)))))))
-    (println "Everything sent.")
     pagent))
 
 (defn- await-process [pagent]
@@ -75,11 +74,9 @@
 (defn run-task [pagent]
   (let [execp @pagent
         status (await-process pagent)]
-    (do (println (str "Finshed job at " (:machine execp)))
-        (assoc execp :status status))))
+    (assoc execp :status status)))
 
 (defn send-to-machine [machine task]
-  (println (str "Starting " task " at " machine))
   (spawn machine
          (into-array String
                      ["ssh"
