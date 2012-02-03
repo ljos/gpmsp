ef74d0bec0e0d1fdbbf6e71b6b37a24bab86a5b8
Bjarte Johansen <bjo013@uib.no>
2012-02-03 20:33:52 +0100

    Need something to test connections

--- control.clj
+++ control.clj
@@ -96,6 +96,49 @@
               ;;"mn121040"
               ])
 
+(defcluster :all
+  :parallel false
+  :use "bjo013"
+  :ssh-options ["-o PubkeyAuthentication=no" "-o PasswordAuthentication=yes"]
+  :addresses ["mn121033" "mn121034"	
+              "mn121035" "mn121036"
+              "mn121037" "mn121038"
+              "mn121039" "mn121040"	
+              "mn121041" "mn121042"	
+              "mn121043" "mn121044"	
+              "mn121045" "mn121046"	
+              "mn121047" "mn121048"	
+              "mn121049" "mn121050"	
+              "mn121051" "mn121052"	
+              "mn121053" "mn121054"
+              "mn121055" "mn121056"	
+              "mn121057" "mn121058"	
+              "mn121069" "mn121071"	
+              "mn121072" "mn121073"	
+              "mn121074" "mn121075"	
+              "mn121077" "mn121083"	
+              "mn121085" "mn121086"	
+              "mn121087" "mn121088"	
+              "mn121090" "mn121091"	
+              "mn121092" "mn121093"	
+              "mn121094" "mn121095"	
+              "mn121096" "mn121097"	
+              "mn121098" "mn121099"	
+              "mn121100" "mn121101"	
+              "mn121102" "mn121103"	
+              "mn121104" "mn121105"	
+              "mn121107" "mn190142"	
+              "mn190143" "mn190144"	
+              "mn190145" "mn190146"	
+              "mn190147" "mn190148"	
+              "mn190149" "mn190151"
+              "mn190153" "mn190154"
+              "mn190155" "mn190156"
+              "mn190157"])
+
+(deftast :get-host "Gets gostname" []
+  (ssh (run "hostname")))
+
 (deftask :run-gp "Runs gp on the cluster" []
   (ssh (cd "mspacman"
            (run "~/.scripts/check_for_user; ~/.lein/bin/lein run -m mspacman.gpmsp/run-gen '({:program (do (msp-sleep) (move-down) (and mspacman blinky) (move-right) (move-down)), :fitness 390})'"))))
