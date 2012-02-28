34d8be6eb2ec7e9262da34c9077ea5326a6a4fe5
Bjarte Johansen <bjo013@uib.no>
2012-02-28 17:58:11 +0100

    missing s

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
                  [org.clojure/data.zip "0.1.0"]
                  [control "0.2.4-SNAPSHOT"]]
   :dev-dependencies [[control "0.2.3-SNAPSHOT"]]
-  :java-source-path [["CottAGE_1.6_src/"]
-                     ["MSPCRGP/"]]
+  :java-source-paths [["CottAGE_1.6_src/"]
+                      ["MSPCRGP/"]]
   :jvm-opts ["-Xmx3G"
               "-Xms2G"
               "-Xmn1G"
