7119b2eba6e497ddde18d43205ae33e04bbf1e13
Bjarte Johansen <bjo013@uib.no>
2012-03-07 14:05:58 +0100

    Changed GUIMsPacman to use Countdownlatch like nuimspacman

--- MSPCRGP/no/uib/bjo013/mspacman/test/GUIMsPacmanTest.java
+++ MSPCRGP/no/uib/bjo013/mspacman/test/GUIMsPacmanTest.java
@@ -1,33 +1,41 @@
 package no.uib.bjo013.mspacman.test;
 
 import java.awt.BorderLayout;
+import java.util.concurrent.CountDownLatch;
 
 import javax.swing.JFrame;
 
 import no.uib.bjo013.mspacman.GUIMsPacman;
 
-
 public class GUIMsPacmanTest {
 
 	public static void main(String[] args) throws Exception {
-		GUIMsPacman[] cs = new GUIMsPacman[7];
-		for (int i = 0; i < cs.length; ++i) {
-			GUIMsPacman c = new GUIMsPacman();
-			c.setSize(224, 288 + 22); // I think the + 22 is because of the top bar.
-			cs[i] = c;
-		}
+		CountDownLatch[] signal = { new CountDownLatch(1),
+				new CountDownLatch(1), new CountDownLatch(1) };
+		GUIMsPacman c = new GUIMsPacman(signal);
+		c.setSize(224, 288 + 22); // I think the + 22 is because of the top bar.
 
-		for (int i = 0; i < cs.length; ++i) {
-			JFrame app = new JFrame("Ms. Pacman " + i);
-			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			app.setSize(224, 288 + 22);
-			app.setLocation(240 * (i % 7), 0);
-			app.getContentPane().add(cs[i], BorderLayout.CENTER);
-			app.setVisible(true);
-		}
+		JFrame app = new JFrame("Ms. Pacman");
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setSize(224, 288 + 22);
+		app.setLocation(240, 0);
+		app.getContentPane().add(c, BorderLayout.CENTER);
+		app.setVisible(true);
+		Thread t = new Thread(c);
+		t.start();
 
-		for (GUIMsPacman c : cs) {
-			new Thread(c).start();
+		System.out.println("before await");
+		signal[0].await();
+		System.out.println("before await");
+		signal[1].await();
+		System.out.println("before await");
+
+		signal[2].await();
+
+		synchronized (c) {
+			c.stopMSP();
 		}
+		t.join();
+		System.out.println(t.isAlive());
 	}
 }
