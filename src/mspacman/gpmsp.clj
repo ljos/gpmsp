055886c9eafaaf3e69e95ad6b0c1a711f977bdae
Bjarte Johansen <bjo013@uib.no>
2012-04-02 15:26:33 +0200

    changing some things and trying to run on only one machine

--- src/mspacman/gpmsp.clj
+++ src/mspacman/gpmsp.clj
@@ -9,7 +9,7 @@
 
 (defstruct individual :program :fitness)
 
-(def SIZE-OF-POPULATION 200)
+(def SIZE-OF-POPULATION 100)
 (def ELITISM-RATE 0.05)
 (def NUMBER-OF-GENERATIONS 1000)
 (def MAX-STARTING-DEPTH 10)
