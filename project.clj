1f0890bd5f94189d4133fb626e2bad6bd07f35bb
Bjarte Johansen <bjo013@uib.no>
2012-04-17 14:15:52 +0200

    Changed to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mspacman "1.0.0-SNAPSHOT"
   :description "Genetic programming of mspacman bot"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.0"]
                  [org.clojure/data.priority-map "0.0.1"]]
   :java-source-path "./" 
