544f962b07fbe3b4642c3d0558160f9f5cd45a92
Bjarte Johansen <bjo013@uib.no>
2012-05-08 14:29:33 +0200

    Added logging, removed priority-map dep

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Genetic programming of mspacman bot"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.0"]
-                 [org.clojure/data.priority-map "0.0.1"]]
+                 [org.clojure/tools.logging "0.2.3"]]
   :java-source-path "./" 
   :jvm-opts ["-Xmx3G"
              "-Xms2G"
